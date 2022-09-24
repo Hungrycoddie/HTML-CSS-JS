4f71db885ff260f8bfa5f5708956f564fc132aa8
HungryCodie <106869558+Hungrycoddie@users.noreply.github.com>
2022-09-24 23:24:54 +0530

    Update script.js

--- BMIcalculator/script.js
+++ BMIcalculator/script.js
@@ -1,9 +1,28 @@
 function BMI() 
 {
-    let height = document.getElementById("h").value;
-    let weight = document.getElementById("w").value;
-    let bmi = weight / (((height / 100) * height) / 100);
-    let bmio = bmi.toFixed(2);
-  
-    document.getElementById("result").innerHTML = "Your BMI is " + bmio;
+    let h = document.getElementById('h').value;
+    let w = document.getElementById('w').value;
+
+    if (h == "" && w == "") {
+        document.getElementById('result').innerHTML = "Please fill the above fields";
+    } else {
+
+        let result = w / (h / 100) / (h / 100);
+
+        result = result.toFixed(2);
+        document.getElementById('result').innerHTML = result;
+
+        if (result < 18.5) {
+            document.getElementById('result').innerHTML = `${result} Underweight`;
+        }
+        if (result >= 18.5 && result <= 24.9) {
+            document.getElementById('result').innerHTML = `${result} Normal Weight`;
+        }
+        if (result >= 25 && result <= 29.9) {
+            document.getElementById('result').innerHTML = `${result} Overweight`;
+        }
+        if (result >= 30) {
+            document.getElementById('result').innerHTML = `${result} Obesity`;
+        }
+    }
 }
